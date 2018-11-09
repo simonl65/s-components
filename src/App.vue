fe79232353d23c2f5e001804e524a045d4f91b51
Simon Lincoln <git@codeability.co.uk>
2018-11-09 23:30:18 +0000

    Reset all via $refs

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+        <Counter
+            ref="Counter1"
+            class="col-sm-6 my-2 counter"
+        />
+        <Counter
+            ref="Counter2"
+            class="col-sm-6 my-2 counter"
+        />
+    </div>
+    <div class="row">
+        <button
+            @click="resetAll"
+            class="btn btn-block btn-danger"
+        >RESET ALL</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Counter from "./components/Counter.vue";
 
 export default {
-  name: "app",
   components: {
-    HelloWorld
+    Counter
+  },
+
+  data() {
+    return {
+      reset: null
+    };
+  },
+
+  methods: {
+    resetAll() {
+      console.log("reset sent");
+      this.$refs.Counter1.resetCount();
+      this.$refs.Counter2.resetCount();
+    }
   }
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
 </style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="counter">
+        <div class="count--current"
+        >{{ currentCount }}</div>
+            <button class="btn col-6 btn-danger" @click="dec">-</button>
+            <button class="btn col-6 btn-success" @click="inc">+</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "counter",
+
+  data() {
+    return {
+      currentCount: 0
+    };
+  },
+
+  props: ["reset"],
+
+  methods: {
+    inc() {
+      this.currentCount++;
+    },
+
+    dec() {
+      this.currentCount--;
+    },
+
+    resetCount() {
+      console.log("reset received");
+      this.currentCount = 0;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.counter {
+  text-align: center;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 1rem;
+  padding-top: 0;
+}
+
+.count--current {
+  font-size: 3rem;
+}
+</style>
